38bf838305a5063077c7488d4f24d1224d13e735
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-12 15:37:01 -0300

    v1.4

---
+++ src/main/kotlin/br/com/alura/bytebank/exception/SaldoInsuficienteException.kt
@@ -0,0 +1,7 @@
+package br.com.alura.bytebank.exception
+
+/**
+ *@Author giovanni.moratto
+ */
+
+class SaldoInsuficienteException : Exception("O saldo é insuficiente")
--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,6 +1,6 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.exception.SaldoInsuficienteException
 
 /**
  *@Author giovanni.moratto
@@ -16,9 +16,9 @@ fun funcao1() {
     println("início funcao1")
     try {
         funcao2()
-    } catch (e: ClassCastException) {
+    } catch (e: SaldoInsuficienteException) {
         e.printStackTrace()
-        println("ClassCastException foi pegada")
+        println("SaldoInsuficienteException foi pegada")
     }
     println("fim funcao1")
 }
@@ -27,8 +27,7 @@ fun funcao2() {
     println("início funcao2")
     for (i in 1..5) {
         println(i)
-        val endereco = Any()
-        throw ArithmeticException()
+        throw SaldoInsuficienteException()
     }
     println("fim funcao2")
 }
